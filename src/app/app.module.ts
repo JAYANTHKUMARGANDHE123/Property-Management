e8cead2b7d006b1ec2eb9602e14e2d89ec9b3517
jayanthkumar <jayanthkumargandhe@gmail.com>
2024-08-04 18:55:09 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CreatePropertiesComponent } from './create-properties/create-properties.component';
+import { PropertiesComponent } from './properties/properties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreatePropertiesComponent,
+    PropertiesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/create-properties/create-properties.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-properties',
+  templateUrl: './create-properties.component.html',
+  styleUrl: './create-properties.component.css'
+})
+export class CreatePropertiesComponent {
+
+}
